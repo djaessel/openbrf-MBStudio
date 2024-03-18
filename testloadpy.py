f89983103b402fddfe5e7ac70ae8e9c946b65204
Johandros <kochjohannes98@gmail.com>
2024-03-18 18:22:06 +0100

    club test case (WIP)

--- testloadpy.py
+++ testloadpy.py
@@ -1,12 +1,42 @@
+import time
+import threading
 from ctypes import *
-lib = CDLL("./libopenBrf.so")
 
-def checkx():
-    for func_name in dir(lib):
-        print(func_name)
+def main():
+    lib = CDLL("./libopenBrf.so")
+    
+    x = threading.Thread(target=start_lib, args=(lib,))
+    x.start()
 
-args = (c_char_p * 1)()
-args[0] = b"test"
+    #y = threading.Thread(target=extra, args=(lib,))
+    #y.start()
+    
+    extra(lib)
 
-lib.StartExternal(len(args), args)
-#checkx()
+
+def start_lib(lib):
+    args = (c_char_p * 1)()
+    args[0] = b""
+    lib.StartExternal(len(args), args)
+
+
+def extra(lib):
+    time.sleep(5)
+
+    lib.SetModPath(b"/home/djaessel/.steam/debian-installation/steamapps/common/MountBlade Warband/Modules/NordInvasion/")
+
+    print("GET READY")
+
+    time.sleep(5)
+
+    name = b"club"
+    kind = 0
+    lib.SelectItemByNameAndKind(name, kind)
+
+    time.sleep(5)
+
+    lib.SelectItemByNameAndKind(name, kind)
+
+
+main()
+
